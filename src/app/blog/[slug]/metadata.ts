42b51515614aac9c75217977d1918e838b96d2e3
Arthur <tangjiahao@tringame.net>
2025-05-08 13:51:38 +0800

    更新网站 Thu, May  8, 2025  1:51:37 PM

--- src/app/blog/[slug]/metadata.ts
+++
@@ -1,8 +0,0 @@
-
-import { generateBlogPostMetadata, generateBlogJsonLdData } from '@/lib/seo/blog-seo-config';
-
-// Export the metadata generation function
-export { generateBlogPostMetadata as generateMetadata };
-
-// Export the JSON-LD generation function
-export { generateBlogJsonLdData as generateMetadataJsonLd };
